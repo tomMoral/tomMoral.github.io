c8f05a82edd6c984117f46f49e069d06daac66a3
tommoral <thomas.moreau.2010@gmail.com>
2025-10-06 03:37:55 +0200

    FIX add grants page

--- mk_static.py
+++ mk_static.py
@@ -3,22 +3,23 @@ import asyncio
 import aiohttp
 import aiofiles as aiof
 import os
+import json
 
 
 EXPORT_DIR = "."
 STATIC_DIR = "../tommoral.django.local/CV/static/CV"
 
 
-async def fetch_page(session, url, fname):
+async def fetch_page(session, url, fname=None):
     print(url)
     async with session.get(url) as response:
         assert response.status == 200
         content = await response.read()
         content = content.replace(b"/static/CV", b"")
-        async with aiof.open(os.path.join(EXPORT_DIR, fname), mode='wb') as f:
-            await f.write(content)
+        if fname is not None:
+            async with aiof.open(os.path.join(EXPORT_DIR, fname), mode='wb') as f:
+                await f.write(content)
         return content
-    print(url)
 
 
 async def sync_static_files(fname):
@@ -43,6 +44,10 @@ async def sync_images():
 
 async def main():
     async with aiohttp.ClientSession() as session:
+        grants = await fetch_page(
+            session, "http://127.0.0.1:8000/list_grants/"
+        )
+        print(grants)
         await fetch_page(session, 'http://127.0.0.1:8000/about/',
                          "about.html"),
         await fetch_page(session, 'http://127.0.0.1:8000/publications/',
@@ -58,6 +63,11 @@ async def main():
         await sync_static_files("css/style.css"),
         await sync_static_files("javascript/lib.js"),
         await sync_images()
+        grants = json.loads(grants)['grants']
+        for g in grants:
+            await fetch_page(
+                session, f"http://127.0.0.1:8000/{g}/", f"{g}.html"
+            )
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
